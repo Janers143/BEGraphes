62d0c21a325b238591e2b4fa0fffc4a1293ce053
AlexJavor <aleksanjavor@gmail.com>
2019-04-04 12:13:14 +0200

    debut FastestPath

--- src/main/org/insa/graph/Path.java
+++ src/main/org/insa/graph/Path.java
@@ -30,14 +30,21 @@ public class Path {
      * 
      * @throws IllegalArgumentException If the list of nodes is not valid, i.e. two
      *         consecutive nodes in the list are not connected in the graph.
-     * 
-     * @deprecated Need to be implemented.
      */
-    public static Path createFastestPathFromNodes(Graph graph, List<Node> nodes)
-            throws IllegalArgumentException {
-        List<Arc> arcs = new ArrayList<Arc>();
-        // TODO:
+	
+    public static Path createFastestPathFromNodes(Graph graph, List<Node> nodes) {
+        
+    	List<Arc> arcs = new ArrayList<Arc>();
+        
+        Iterator<Arc> itArcs = arcs.iterator();
+        Iterator<Node> itNodes = nodes.iterator();
+        
+		while (itNodes.hasNext()) {
+			
+		}
         return new Path(graph, arcs);
+        
+        //throws IllegalArgumentException {}
     }
 
     /**
